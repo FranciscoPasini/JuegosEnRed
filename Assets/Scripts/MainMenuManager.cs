5f74573be5e4ca1ca1d4d3ac07dabdc14f9e31f9
FranciscoPasini <164237310+FranciscoPasini@users.noreply.github.com>
2025-08-25 21:05:21 -0300

    Connect to NextScene
    - Se le pide al jugador ingresar un nombre y este al ingresar a la siguiente scene se conecta al master y sale por consola

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,15 +2,63 @@ using System.Collections.Generic;
 using Photon.Pun;
 using Photon.Realtime;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
-public class MainMenuManager : MonoBehaviour
+public class MainMenuManager : MonoBehaviourPunCallbacks
 {
-    public void PlayButton()
+    public string gameSceneName;
+    public TMP_InputField nickNameInputField;
+    private const string nicknamekey = "playerNickname";
+    private string nickname;
+    public Button connectionButton;
+
+    private void Start()
     {
+        connectionButton.onClick.AddListener(HandleConnectButton);
+        nickNameInputField.onValueChanged.AddListener(VerifyName);
+    }
+
+    private void VerifyName(string name)
+    {
+        if(nickNameInputField.text.Length == 0) 
+        {
+            connectionButton.interactable = false;
+        }
+
+        if (nickNameInputField.text.Length >= 1 && !connectionButton.interactable)
+        {
+            connectionButton.interactable = true;
+        }
+
+        nickname = name;
+    }
+
+    public void HandleConnectButton()
+    {
+        PlayerPrefs.SetString(nicknamekey, nickname);
+        print(message:nickname + " is trying to conncet");
+        PhotonNetwork.ConnectUsingSettings();
+
+        connectionButton.interactable = false;
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(nickname + " connected to server");
         SceneManager.LoadScene("Levels");
     }
 
+    public void PlayButton()
+    {
+        OnConnectedToMaster();
+    }
 }
