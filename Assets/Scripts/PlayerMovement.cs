c03fe941dcffe3b74fe62a4bbfa5b7ddda565e76
FranciscoPasini <164237310+FranciscoPasini@users.noreply.github.com>
2025-08-31 23:42:53 -0300

    PlayerAppearence

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using Photon.Pun;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int speed = 5;
+    private PhotonView photonView;
+    public PhotonView PhotonView => photonView ?? GetComponent<PhotonView>();
+
+    public void Start()
     {
-        
+       photonView = GetComponent<PhotonView>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (PhotonView.IsMine)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            Vector2 movement = new Vector2(horizontal, vertical);
+            transform.Translate(translation: movement.normalized * speed * Time.deltaTime);
+        }
     }
 }
